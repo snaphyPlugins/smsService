0881633646f519db3091d5ed2042e76ff1b0ebaf
robinskumar73 <robinskumar73@gmail.com>
2016-03-08 18:12:51 +0530

    First commit

---
+++ client/scripts/controllers/smsService.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module($snaphy.getModuleName())
+
+//Controller for smsServiceControl ..
+.controller('smsServiceControl', ['$scope', '$stateParams', 'Database',
+    function($scope, $stateParams, Database) {
+        //Checking if default templating feature is enabled..
+        var defaultTemplate = $snaphy.loadSettings('smsService', "defaultTemplate");
+        $snaphy.setDefaultTemplate(defaultTemplate);
+        //Use Database.getDb(pluginName, PluginDatabaseName) to get the Database Resource.
+    }//controller function..
+]);
---
+++ readMe.md
@@ -0,0 +1,12 @@
+# smsService plugin for Snaphy
+
+
+###A plugin for sending sms
+
+###This plugin is exposed on  `/smsService` route
+
+
+
+
+####Written by Robins Gupta
+
