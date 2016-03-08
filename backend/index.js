bde7bcc5cfc78ca2e66ddd69643a9b9afdf1357b
robinskumar73 <robinskumar73@gmail.com>
2016-03-08 18:17:37 +0530

    Sms implemented

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,60 @@
 'use strict';
-module.exports = function( server, databaseObj, helper, packageObj) {
-	/**
-	 * Here server is the main app object
-	 * databaseObj is the mapped database from the package.json file
-	 * helper object contains all the helpers methods.
-	 * packegeObj contains the packageObj file of your plugin. 
-	 */
+module.exports = function(server, databaseObj, helper, packageObj) {
+    /**
+     * Here server is the main app object
+     * databaseObj is the mapped database from the package.json file
+     * helper object contains all the helpers methods.
+     * packegeObj contains the packageObj file of your plugin.
+     */
 
-	/**
-	 * Initialize the plugin at time of server start.
-	 * init method should never have any argument
-	 * It is a constructor and is populated once the server starts.
-	 * @return {[type]} [description]
-	 */
-	var init = function(){
-
-	};
+    /**
+     * Initialize the plugin at time of server start.
+     * init method should never have any argument
+     * It is a constructor and is populated once the server starts.
+     * @return {[type]} [description]
+     */
+    var init = function() {
 
 
-	//return all the methods that you wish to provide user to extend this plugin.
-	return {
-		init: init
-	}
+    };
+
+
+    var send = function(message, number, callback) {
+        //matching the number..
+        var patt = /\+\d{12,12}/;
+        var match = number.match(patt);
+        if (!match) {
+            number = "+91" + number;
+        }
+
+        var apiKey = "a8147ba9";
+        var apiSecret = "14ba38cb";
+        var data = 'https://rest.nexmo.com' +
+            '/sms/json?api_key=' + apiKey + '&api_secret=' + apiSecret +
+            '&from=Gruberr&to=' + number +
+            '&text=' + message;
+        https.get(
+            data,
+            function(res) {
+                res.on('data', function(data) {
+                    // all done! handle the data as you need to
+                    console.log("Message sent");
+                    //console.log(data);
+                    callback(null, data);
+                });
+            }
+        ).on('error', function(err) {
+            console.log("Error sending push message to the server.");
+            //console.error(err);
+            // handle errors somewhow
+            callback(err, null);
+        });
+    };
+
+
+    //return all the methods that you wish to provide user to extend this plugin.
+    return {
+        init: init,
+        send: send
+    }
 }; //module.exports
